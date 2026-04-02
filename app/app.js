f8ce8e3fa628f3c2c2140be89edb481409a70ba9
yashmishraaaa <yashmishramishra401@gmail.com>
2026-04-02 16:07:54 +0530

    test

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('🚀 DevOps Capstone Running Successfully!');
+    res.send('DevOps Capstone Running Successfully!');
 });
 
 app.get('/health', (req, res) => {
@@ -11,4 +11,4 @@ app.get('/health', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
-});
+});  
